a8b8399b47bf986cf5d8b273748865f81381a3c9
Mr-Timur <tymur.mirzada@gmail.com>
2020-04-17 18:56:11 +0300

    Remove empty locales

--- src/utils/to-folders.js
+++ src/utils/to-folders.js
@@ -32,7 +32,7 @@ function parseTranslation(json, rootFolder, options, acc = 0) {
 
       json = omit(json, key);   // deletes the folder key so that we are
     } else {
-      fs.writeFileSync(`${targetFolder}.yml`, jsonToYml(json[key]));
+      fs.writeFileSync(`${targetFolder}.yml`, jsonToYml(removeNullValues(json[key]) || {}));
       json = omit(json, key);   // deletes the folder key so that we are
     }
   }
@@ -48,4 +48,21 @@ function parseTranslation(json, rootFolder, options, acc = 0) {
 export default function toFolders(rootFolder, target, locale, options) {
   const strippedTarget = ymlToJson(target)[locale];
   return parseTranslation(strippedTarget, rootFolder, options);
+}
+
+const removeNullValues = (object) => {
+  if (!object || typeof object !== 'object') return object
+  const newObject = {}
+
+  Object.keys(object).forEach(key => {
+    if (object[key] && typeof object[key] === 'object') {
+      newObject[key] = removeNullValues(object[key])
+    } else if (object[key] != null) {
+      newObject[key] = object[key]
+    }
+
+    if (newObject[key] && typeof newObject[key] === 'object' && Object.keys(newObject[key]).length === 0) delete newObject[key]
+  })
+
+  return newObject
 }
